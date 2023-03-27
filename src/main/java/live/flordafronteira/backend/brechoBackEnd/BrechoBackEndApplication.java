61c88a11b26287cbcda5534196b533e106700102
Administrador <thhyagofoz@gmail.com>
2023-03-27 15:55:12 -0300

    comitando

---
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/BrechoBackEndApplication.java
@@ -0,0 +1,13 @@
+package live.flordafronteira.backend.brechoBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrechoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrechoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/flordafronteira/backend/brechoBackEnd/BrechoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package live.flordafronteira.backend.brechoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrechoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

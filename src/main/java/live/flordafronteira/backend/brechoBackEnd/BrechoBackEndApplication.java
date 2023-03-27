17d7107ee435214552653511b101d42dab449344
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-03-27 20:06:19 -0300

    feito a classe de tipo de pruduto

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/BrechoBackEndApplication.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/BrechoBackEndApplication.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 @SpringBootApplication
 public class BrechoBackEndApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BrechoBackEndApplication.class, args);
 

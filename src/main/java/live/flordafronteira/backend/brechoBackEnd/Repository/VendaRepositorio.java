7cdcf706484fc0ef0bbf28bfd310f119ab1f3f4f
Administrador <thhyagofoz@gmail.com>
2023-05-01 11:54:05 -0300

    ajsutado o nome da coluna co_tipo_produto na classe produto

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/Repository/VendaRepositorio.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/Repository/VendaRepositorio.java
@@ -4,6 +4,6 @@ import live.flordafronteira.backend.brechoBackEnd.Entity.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository//quando for chave composta é indicado fazer uma classe e informar aqui
 public interface VendaRepositorio extends JpaRepository <Venda, Long> {
 }

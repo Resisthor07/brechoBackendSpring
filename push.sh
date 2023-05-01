6e292cb57e2008736d5289400b77fbfdc2691ddb
SamirCharleston <samircharleston@gmail.com>
2023-05-01 12:40:45 -0300

    salvando -

--- push.sh
+++ push.sh
@@ -1,4 +1,11 @@
 git add .
-git commit -m "salvando -- $conteudo"
+if [ conteudo != "" ];
+then
+	git commit -m "salvando - $conteudo"
+	echo "Tentativa de envio de $conteudo"
+else
+	git commit -m "salvando"
+	echo "Tentativa de envio realizada"
+fi
 git push origin $nome
 

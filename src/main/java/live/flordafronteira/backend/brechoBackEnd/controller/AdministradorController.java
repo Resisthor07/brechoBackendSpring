aa7b57750e261b61c57b87d11ad974b5ae343dae
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-05-02 16:09:00 -0300

    salvando

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/AdministradorController.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/AdministradorController.java
@@ -1,11 +1,14 @@
 package live.flordafronteira.backend.brechoBackEnd.controller;
 
+import live.flordafronteira.backend.brechoBackEnd.entity.Administrador;
+import live.flordafronteira.backend.brechoBackEnd.entity.Produto;
 import live.flordafronteira.backend.brechoBackEnd.repository.AdministradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping (value = "api/administrador")
@@ -19,4 +22,14 @@ public class AdministradorController {
 
         return ResponseEntity.ok(this.administradorRepository.findAll());
     }
+
+    @GetMapping
+    public ResponseEntity<?> findByRequest(@RequestParam("id")final Long id){
+        final Administrador administrador = this.administradorRepository.findById(id).orElse(null);
+        return administrador == null
+                ? ResponseEntity.badRequest().body("Nenhum produto encontrado!")
+                : ResponseEntity.ok(administrador);
+    }
+
+
 }

d2625e741f954162dcd991147f5e9631a333ac0c
SamirCharleston <samircharleston@gmail.com>
2023-05-02 10:16:28 -0300

    salvando

--- push.sh
+++ push.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 git add .
-if [ -z "$mensagem" ];
+echo -e "Digite o nome da branch\n"
+read BRANCH
+if [ -z $MSG ];
 then
 	git commit -m "salvando"
 	echo "Tentativa de envio"
 else
-	git commit -m "salvando - $mensagem"
-	echo "Tentativa de envio realizada - $mensagem"
+	git commit -m "salvando - $MSG"
+	echo "Tentativa de envio realizada - $MSG"
 fi
-git push origin developer
+git push origin $BRANCH
 

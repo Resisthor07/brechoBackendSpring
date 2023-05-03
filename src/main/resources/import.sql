aca7b492f76830925fff186e222e0c7015266383
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-05-02 21:46:12 -0300

    inserindo por hoje

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,7 @@
 --Samir
 
 --Eduardo
-insert into tb_administrador (id,dt_criacao,dt_atualizacao,st_ativo,no_email,no_login,no_administrador,no_senha)
-VALUES (1,'10-01-2020 10:50:10', '11-01-2020 10:55:10', true,'brechoflor@gmail.com', 'yane','Yane Prado','1234');
+insert into tb_administrador (id,dt_criacao,dt_atualizacao,st_ativo,no_email,no_login,no_administrador,no_senha)VALUES (1,'10-01-2020 10:50:10', '11-01-2020 10:55:10', true,'brechoflor@gmail.com', 'yane','Yane Prado','1234');
 
 --Tailine
 

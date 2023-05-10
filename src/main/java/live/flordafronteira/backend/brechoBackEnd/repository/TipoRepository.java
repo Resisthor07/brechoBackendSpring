cd076d0a40a1cd95a93ef743df140f69ec0aea00
Rafael Ch <rafael.cherman0@gmail.com>
2023-05-10 14:25:07 -0300

    Modificado o nome

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/TipoRepository.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/TipoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TipoRepositorio extends JpaRepository<Tipo, Long> {
+public interface TipoRepository extends JpaRepository<Tipo, Long> {
 }

a3df0a38c282d93c2fd9f6a45a0ec0bcc2db3788
Rafael Ch <rafael.cherman0@gmail.com>
2023-05-10 13:59:48 -0300

    Criado TipoService herdando AbstrataService e implementando seus metodos

---
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/service/TipoService.java
@@ -0,0 +1,39 @@
+package live.flordafronteira.backend.brechoBackEnd.service;
+
+
+import live.flordafronteira.backend.brechoBackEnd.abstractClasses.AbstrataService;
+import live.flordafronteira.backend.brechoBackEnd.entity.Tipo;
+import live.flordafronteira.backend.brechoBackEnd.repository.TipoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TipoService extends AbstrataService<TipoRepository, Tipo> {
+
+
+    @Override
+    public String validaObjeto(Tipo objetoParam) {
+        return null;
+    }
+
+    @Override
+    public boolean consultaEmBancoVinculoComTabelas(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Tipo> consultaEmBancoAtivos() {
+        return null;
+    }
+
+    @Override
+    public Tipo aplicaRegrasDeNegocio(Tipo objetoParam) {
+        return null;
+    }
+
+    @Override
+    public Tipo filtraDados(Tipo objetoParam) {
+        return null;
+    }
+}

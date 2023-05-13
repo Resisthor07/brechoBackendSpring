ffebf6ea8fa1fd93cb723253a3646f859551dcbe
Rafael Ch <rafael.cherman0@gmail.com>
2023-05-13 13:04:40 -0300

    Criado Query isInProduto

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/TipoRepository.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/TipoRepository.java
@@ -2,8 +2,13 @@ package live.flordafronteira.backend.brechoBackEnd.repository;
 
 import live.flordafronteira.backend.brechoBackEnd.entity.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TipoRepository extends JpaRepository<Tipo, Long> {
+
+    @Query("select exists (select p from Produto p where p.tipo.id = :id)")
+    boolean isInProduto(@Param("id") final Long id);
 }

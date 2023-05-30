b637d3a77069b0e094ef175677a41b5406b949d4
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-05-30 19:28:29 -0300

    Alterando anotações

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/TipoController.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/TipoController.java
@@ -7,9 +7,10 @@ import live.flordafronteira.backend.brechoBackEnd.service.TipoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "api/tipo")
+@RestController
+@RequestMapping(value = "/api/tipo")
 public class TipoController extends AbstrataController<TipoService, Tipo> {
 
     @Autowired

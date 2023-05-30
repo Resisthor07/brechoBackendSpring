6b61f0455e3afb6189b2b786dffb05671999b5d4
Thiago Cavalcante <116036410+Thiago-cavalcante-oliveira@users.noreply.github.com>
2023-05-30 16:14:28 -0300

    ajustei a entidade na parte do atributo da data de edicao, pois estava com condição notnull, e não rodava. Fiz as anotations de validação da classe cliente

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/ClienteController.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/controller/ClienteController.java
@@ -1,4 +1,5 @@
 package live.flordafronteira.backend.brechoBackEnd.controller;
+import live.flordafronteira.backend.brechoBackEnd.abstractClasses.AbstrataController;
 import live.flordafronteira.backend.brechoBackEnd.entity.Cliente;
 import live.flordafronteira.backend.brechoBackEnd.repository.ClienteRepositorio;
 
@@ -16,59 +17,5 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/api/cliente")
-public class ClienteController {
-    @Autowired
-    private ClienteService clienteService;
-    @Autowired
-    private ClienteRepositorio clienteRepositorio;
-
-    @GetMapping
-    public ResponseEntity<?> buscaID(@RequestParam("id") final Long id) {
-        final Cliente valorBanco = this.clienteRepositorio.findById(id).orElse(null);
-        return valorBanco == null ? ResponseEntity.badRequest().body("Nenhum Cliente corresponde ao ID informado") :
-                ResponseEntity.ok(valorBanco);
-    }
-
-    @GetMapping("/clientesativos")
-    public ResponseEntity<?> buscaListaClientesAtivos() {
-        return ResponseEntity.ok(this.clienteRepositorio.listaClienteAtivo());
-    }
-
-
-    @GetMapping("/listaclientes")
-    public ResponseEntity<?> buscaListaClientes() {
-        return ResponseEntity.ok(this.clienteRepositorio.findAll());
-    }
-
-    @PostMapping
-    public ResponseEntity<?> cadastraCliente(@RequestBody final Cliente cliente) {
-        try {
-            clienteService.cadastraCliente(cliente);
-            return ResponseEntity.ok("Cliente cadastrado com sucesso");
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> atualizaClienteCompleto(
-            @PathVariable final Long id,
-            @RequestBody final Cliente cliente) {
-        try {
-            clienteService.atualizarCliente(id, cliente);
-            return ResponseEntity.ok("Cliente Atualizado.");
-        } catch (RuntimeException e) {
-            return ResponseEntity.internalServerError().body("Erro: " + e.getMessage());
-        }
-    }
-
-   @DeleteMapping("/{id}")
-    public ResponseEntity<?> DeletarCliente(@RequestParam("id") final Long id) {
-       try {
-           clienteService.deletarCliente(id);
-           return ResponseEntity.ok("Cliente deletado.");
-       } catch (RuntimeException e) {
-           return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
-       }
-   }
+public class ClienteController extends AbstrataController<ClienteService, Cliente> {
 }
--- src/main/java/live/flordafronteira/backend/brechoBackEnd/entity/Cliente.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/entity/Cliente.java
@@ -2,8 +2,7 @@ package live.flordafronteira.backend.brechoBackEnd.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.Scanner;
@@ -11,75 +10,51 @@ import org.hibernate.envers.*;
 
 @Entity
 @Audited
-//@AuditTable(value="audit_clientes", schema = "audit")
-@Table(name = "tb_clientes", schema = "public")
+@AuditTable(value="audit_clientes", schema = "audit")
+@Table(name = "tb_clientes", schema = "brecho")
 public class Cliente extends Entidade {
     @Getter @Setter
-    @Column(name = "no_cliente", nullable = false, length = 150)
+    @Column(name = "no-cliente", nullable = false, length = 150)
+    @NotNull(message = "Nome inválido ou nulo.") @NotBlank(message = "Nome inválido ou nulo.")
+    @Size(min = 5, max = 150, message = "Nome inválido, não pode ser inferior a 5 caracteres ou superior a 150 caracteres")
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Nome não pode conter caracteres especiais")
     private String nome;
     @Getter @Setter
-    @Column(name = "num_telefone", nullable = false, length=20)
+    @Column(name = "num-telefone", nullable = false, length=20)
+    @Pattern(regexp = "^[0-9]{2} [0-9]{4,5}-[0-9]{4}$", message = "Telefone inválido.")
     private String telefone;
     @Getter @Setter
-    @Column(name = "no_email", nullable = false, length = 150, unique = true)
+    @Column(name = "no-email", nullable = false, length = 150, unique = true)
     @Email(message = "email inválido.")
     @Size(max = 150, message = "O email não pode ultrpassar 150 caracteres")
     private String email;
     @Getter @Setter
+    @Pattern(regexp = "^[0-9]{5}-[0-9]{3}$", message = "CEP inválido.")
+    @NotNull(message = "CEP não informado.") @NotBlank(message = "CEP não informado.") @NotEmpty(message = "CEP não informado.")
+
     @Column(name = "num_cep", nullable = false, length = 10)
+
     private String cep;
     @Getter @Setter
-    @Column(name = "tp_logradouro", nullable = false)
+    @Column(name = "tp-logradouro", nullable = false)
     @Enumerated(EnumType.STRING)
     private TipoDeLogradouro tipoLogradouro;
     @Getter @Setter
-    @Column(name = "no_logradouro", nullable = false, length = 150)
+    @Column(name = "no-logradouro", nullable = false, length = 150)
     @Size(min = 1, max = 150, message = "O nome do logradouro não pode ser menor que um caracter e não pode ultrapassar 150 caracteres.")
     private String logradouro;
     @Getter @Setter
-    @Column(name = "num_residencia", length=10)
+    @Column(name = "num-residencia", length=10)
+    @NotNull(message = "Nº inválido") @NotBlank(message = "Nº inválido") @NotEmpty(message = "Nº inválido")
+    @Size(max = 10, message = "Não pode ultrapassar 10 caracteres")
     private String numResidencia;
     @Getter @Setter
-    @Column(name = "no_bairro", nullable = false, length = 150)
+    @Column(name = "no-bairro", nullable = false, length = 150)
+    @NotNull(message = "Bairro inválido") @NotBlank(message = "Bairro inválido") @NotEmpty(message = "Bairro inválido")
+    @Size(min = 10, max = 150, message = "Nome do bairro não pode ser inferiro a 10 caracteres nem superior a 150.")
     private String bairro;
     @Getter @Setter
-    @Column(name = "ds_ptreferencia", length = 300)
+    @Column(name = "ds-ptreferencia", length = 300)
     private String pontoReferencia;
-
-
-    public boolean addCliente(){
-        Scanner ler = new Scanner(System.in);
-        boolean repete = false;
-        System.out.println("Informe seu nome:");
-        nome= ler.next();
-        ler.nextLine();
-        System.out.println("Informe o seu email:");
-        email= ler.next();
-        ler.nextLine();
-        System.out.println("Informe seu logradouro:");
-        logradouro= ler.next();
-        ler.nextLine();
-        System.out.println("Informe seu bairro:");
-        bairro= ler.next();
-        ler.nextLine();
-        System.out.println("Gostaria de cadastrar um novo cliente?\n (S) Sim (N) Não:");
-        repete = newCliente(ler.next());
-        ler.nextLine();
-        return repete;
-
-    }
-
-    public boolean newCliente(String add){
-        if(add.equalsIgnoreCase("S")){
-            return true;
-        }else return false;
-    }
-
-     /*   public ArrayList<Cliente> listarCliente(ArrayList<Cliente> listar){
-            for (Cliente valor:listar) {
-                System.out.println("Cliente:"+ valor.getNome() + "\nEmail:"+ valor.getEmail());
-
-            }
-        }*/
 
 }

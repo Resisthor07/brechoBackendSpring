cd85254b12dd47865bec54987459a4563274b539
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-07-04 16:50:57 -0300

    inserido o administrador automatico e autenticando com o front

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/AdministradorRepository.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/AdministradorRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
     Administrador findByLogin(String login);
+
+
 }

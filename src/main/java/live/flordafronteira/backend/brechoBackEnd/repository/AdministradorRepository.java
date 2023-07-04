51c7bd0a2509853c742e88b60f4ea078b8f98d6c
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-07-04 14:30:51 -0300

    iniciando configuracao administrador

--- src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/AdministradorRepository.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/repository/AdministradorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-
+    Administrador findByLogin(String login);
 }
--- src/main/java/live/flordafronteira/backend/brechoBackEnd/service/AdministradorService.java
+++ src/main/java/live/flordafronteira/backend/brechoBackEnd/service/AdministradorService.java
@@ -3,13 +3,31 @@ package live.flordafronteira.backend.brechoBackEnd.service;
 import live.flordafronteira.backend.brechoBackEnd.abstractClasses.AbstrataService;
 import live.flordafronteira.backend.brechoBackEnd.entity.Administrador;
 import live.flordafronteira.backend.brechoBackEnd.repository.AdministradorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class AdministradorService extends AbstrataService<AdministradorRepository, Administrador> {
+    private final AdministradorRepository administradorRepository;
+
+    @Autowired
+    public AdministradorService(AdministradorRepository administradorRepository) {
+        this.administradorRepository = administradorRepository;
+    }
+
+    public boolean autenticar(String login, String senha) {
+        Administrador administrador = administradorRepository.findByLogin(login);
+
+        if (administrador != null && administrador.getSenha().equals(senha)) {
+            return true; // Autenticação bem-sucedida
+        } else {
+            return false; // Credenciais inválidas
+        }
+    }
     @Override
     public String validaObjeto(Administrador administrador) {
-        if(administrador.getNome() == null){
+        if (administrador.getNome() == null) {
             return "NOME NULO";
         } else if (administrador.getEmail() == null) {
             return "EMAIL NULO";
@@ -24,6 +42,8 @@ public class AdministradorService extends AbstrataService<AdministradorRepositor
 
     @Override
     public boolean consultaEmBancoVinculoComTabelas(Long id) {
+        // Implemente a lógica de consulta em banco de dados aqui
+        // Verifique se existem vínculos nas tabelas relacionadas ao administrador com o ID fornecido
         return false;
     }
 
@@ -34,11 +54,14 @@ public class AdministradorService extends AbstrataService<AdministradorRepositor
 
     @Override
     public Administrador aplicaRegrasDeNegocio(Administrador administrador) {
+        // Implemente as regras de negócio específicas para administradores aqui
         return administrador;
     }
 
     @Override
     public Administrador filtraDados(Administrador administrador) {
+        // Implemente a lógica de filtragem de dados aqui
+        // Se necessário, filtre e ajuste os dados do administrador antes de serem retornados
         return administrador;
     }
 }
